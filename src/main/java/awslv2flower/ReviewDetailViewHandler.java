d0b6b11f2eb12c68ffd6b97522775a509211b485
dony79 <dony79@icloud.com>
2020-09-16 15:02:42 +0000

    review2

--- src/main/java/awslv2flower/ReviewDetailViewHandler.java
+++ src/main/java/awslv2flower/ReviewDetailViewHandler.java
@@ -1,40 +1,40 @@
-package awslv2flower;
+//package awslv2flower;
 
-import awslv2flower.config.kafka.KafkaProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Service;
+//import awslv2flower.config.kafka.KafkaProcessor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.stream.annotation.StreamListener;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
+//import java.io.IOException;
+//import java.util.List;
+//import java.util.Optional;
 
-@Service
-public class ReviewDetailViewHandler {
+//@Service
+//public class ReviewDetailViewHandler {
 
 
-    @Autowired
-    private ReviewDetailRepository reviewDetailRepository;
+  //  @Autowired
+    //private ReviewDetailRepository reviewDetailRepository;
 
-    @StreamListener(KafkaProcessor.INPUT)
-    public void whenReveiwWrited_then_CREATE_1 (@Payload ReveiwWrited reveiwWrited) {
-        try {
-            if (reveiwWrited.isMe()) {
+    //@StreamListener(KafkaProcessor.INPUT)
+    //public void whenReveiwWrited_then_CREATE_1 (@Payload ReveiwWrited reveiwWrited) {
+      //  try {
+        //    if (reveiwWrited.isMe()) {
                 // view 객체 생성
-                ReviewDetail reviewDetail = new ReviewDetail();
+          //      ReviewDetail reviewDetail = new ReviewDetail();
                 // view 객체에 이벤트의 Value 를 set 함
-                reviewDetail.setOderId(reveiwWrited.getOrderId());
-                reviewDetail.setCustomerName(reveiwWrited.getCustomerName());
-                reviewDetail.setReview(reveiwWrited.getReview());
+            //    reviewDetail.setOderId(reveiwWrited.getOrderId());
+              //  reviewDetail.setCustomerName(reveiwWrited.getCustomerName());
+                //reviewDetail.setReview(reveiwWrited.getReview());
                 // view 레파지 토리에 save
-                reviewDetailRepository.save(reviewDetail);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+             //   reviewDetailRepository.save(reviewDetail);
+    //        }
+    //    }catch (Exception e){
+      //      e.printStackTrace();
+      //  }
+  //  }
 
 
 
-}
+//}
